7ae7935f2df111fa9d5999d1789781feb7184309
Chengyu Lo <betamint@gmail.com>
2013-10-01 05:41:47 +0800

    improve fortune

--- fortune.js
+++ fortune.js
@@ -1,15 +1,16 @@
-var fortune = function (essay) {
+var fortune = function (essay, sep) {
     var len = essay.length;
+    var slen = sep.length;
     var wise = [];
     var index = 0;
 
     for (var i = 0; i < len; i++)
-	if (essay.substr(i, 1) == "%")
+	if (essay.substr(i, slen) == sep)
 	    wise[++index] = i;
-    wise[0] = 0;
+    wise[0] = - slen;
     wise[index+1] = len;
 
     index = parseInt(index * Math.random());
-    wise = essay.substr(wise[index] + 1, wise[index+1] - wise[index] - 1);
+    wise = essay.substr(wise[index] + slen, wise[index+1] - wise[index] - slen);
     return wise;
 };
