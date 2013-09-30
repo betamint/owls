ee79700d239684b99ea47a86d56c278a5a009296
Chengyu Lo <betamint@gmail.com>
2013-10-01 05:18:29 +0800

    symplify fortune

--- fortune.js
+++ fortune.js
@@ -1,26 +1,15 @@
-var fortune = function () {
-    var wisdom = ["/fortune-cowsay/fortunes",
-		 "/fortune-cowsay/literature"];
-    wisdom = wisdom[parseInt (wisdom.length * Math.random())];
+var fortune = function (essay) {
+    var len = essay.length;
+    var wise = [];
+    var index = 0;
 
-    var wise;
-    var ajax = new XMLHttpRequest();
-    ajax.open("GET", wisdom, true);
-    ajax.send();
-    ajax.onreadystatechange = function () {
-	if (ajax.readyState == 4 && ajax.status == 200) {
-	    var len = ajax.responseText.length;
-	    wise = new Array(0);
-	    var number = 0;
-	    for (var i = 0; i < len; i++)
-		if (ajax.responseText.substr(i, 1) == "%")
-		    wise[++number] = i;
-	    wise[0] = 0;
-	    wise[number+1] = len;
-	    number = parseInt(number * Math.random());
-	    wise = ajax.responseText
-		.substr(wise[number] + 1, wise[number+1] - wise[number] - 1);
-	    return wise;
-	}
-    };
+    for (var i = 0; i < len; i++)
+	if (essay.substr(i, 1) == "%")
+	    wise[++index] = i;
+    wise[0] = 0;
+    wise[index+1] = len;
+
+    index = parseInt(index * Math.random());
+    wise = essay.substr(wise[index] + 1, wise[index+1] - wise[index] - 1);
+    return wise;
 };
